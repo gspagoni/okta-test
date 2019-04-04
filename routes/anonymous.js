de6dd9cfc2c602af5ab3cdf76ade8217bacaee24
Giampaolo Spagoni <giampaolo.spagoni@infor.com>
2019-04-04 16:28:05 +0200

    added discovery endpoint

--- routes/anonymous.js
+++ routes/anonymous.js
@@ -17,6 +17,7 @@ var router = express.Router();
  */
 //router.get('/api/puppies', db.getAllPuppies);
 router.get('/test', (req,res) => {
+    console.log(req.headers)
     res.status(200).json({'Message':'Test Success.'})
 })
 
@@ -29,5 +30,47 @@ router.get('/test', (req,res) => {
  *       Message:
  *         type: string
   */
+
+
+ router.post('/v2/discovery', (req, res)=> {
+    const lid = req.body.logicalId;
+    doc = {
+        "logicalId": lid,
+        "supportedDocumentConfigurations": [{
+            "name": "Sync.PurchaseOrder",
+            "receive": true,
+            "send": true
+        },
+        {
+            "name": "Sync.SalesOrder",
+            "receive": true,
+            "send": false 
+        },
+        {
+            "name": "Sync.ItemMaster",
+            "receive": true,
+            "send": false 
+        },
+        {
+            "name": "Process.ItemMaster",
+            "receive": true,
+            "send": true 
+        },
+        {
+            "name": "Get.SalesOrder",
+            "receive": true,
+            "send": true 
+        },
+        {
+            "name": "Get.PurchaseOrder",
+            "receive": true,
+            "send": false 
+        }]
+    }
+
+    
+    res.status(200).json(doc);
+});
+
 
  module.exports = router; 
