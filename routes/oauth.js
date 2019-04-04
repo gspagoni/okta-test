9315de0f916e023dc7db27edcd315d3ee436c8bb
Giampaolo Spagoni <giampaolo.spagoni@infor.com>
2019-04-04 23:07:40 +0200

    added IMS implementation with authentication

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const base64 = require('base-64');
+
 const OktaJwtVerifier = require('@okta/jwt-verifier')
 const oktaJwtVerifier = new OktaJwtVerifier({
   issuer: process.env.ISSUER,
@@ -54,5 +56,91 @@ router.get('/test', checkOAuth, (req,res) => {
  *       Message:
  *         type: string
   */
+
+ router.get('/ping', checkOAuth, (req, res)=> {
+  objret = {
+      "status": "OK",
+      "code": 200,
+      "errors": []
+    }
+  res.status(200).json(objret)
+});
+
+router.get('/versions', checkOAuth, (req, res) => {
+  objret = {
+      "supportedVersions": [
+        "v2"
+      ]
+    }
+    res.status(200).json(objret)
+});
+
+router.get('/protocol', checkOAuth, (req, res) => {
+  objret = {
+      "version"   : "v2",  
+      "messageMethod" : "message",
+      "message_ContentType" : "application/json",
+      "supportedEncoding" : "BASE64",
+      "supportedCharacterSet" : "UTF-8"
+  }
+  res.status(200).json(objret)
+});
+
+
+router.post('/v2/discovery', checkOAuth, (req, res)=> {
+  const lid = req.body.logicalId;
+  doc = {
+      "logicalId": lid,
+      "supportedDocumentConfigurations": [{
+          "name": "Sync.PurchaseOrder",
+          "receive": true,
+          "send": true
+      },
+      {
+          "name": "Sync.SalesOrder",
+          "receive": true,
+          "send": false 
+      },
+      {
+          "name": "Sync.ItemMaster",
+          "receive": true,
+          "send": false 
+      },
+      {
+          "name": "Process.ItemMaster",
+          "receive": true,
+          "send": true 
+      },
+      {
+          "name": "Get.SalesOrder",
+          "receive": true,
+          "send": true 
+      },
+      {
+          "name": "Get.PurchaseOrder",
+          "receive": true,
+          "send": false 
+      }]
+  }
+
+  
+  res.status(200).json(doc);
+});
+
+router.post('/v2/message', checkOAuth, (req, res) => {
+  const objret = {
+      "status" : "OK", 
+      "message" : "Published successfully", 
+      "code" : 201        
+  }
+  const mess = req.body.document.value;
+
+  // code to manage the message goes here
+  
+  console.log(base64.decode(mess));
+
+  res.status(201).json(objret);
+});
+
 
  module.exports = router; 
