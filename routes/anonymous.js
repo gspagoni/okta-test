ceb1267eaabe830f030550f413c570ffbb118318
Giampaolo Spagoni <giampaolo.spagoni@infor.com>
2019-04-04 17:33:23 +0200

    added IMS implementation endpoint

--- routes/anonymous.js
+++ routes/anonymous.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const base64 = require('base-64');
+
 /**
  * @swagger
  * /v1/test:
@@ -30,6 +32,35 @@ router.get('/test', (req,res) => {
  *       Message:
  *         type: string
   */
+
+ router.get('/ping', (req, res)=> {
+    objret = {
+        "status": "OK",
+        "code": 200,
+        "errors": []
+      }
+    res.status(200).json(objret)
+});
+
+router.get('/versions', (req, res) => {
+    objret = {
+        "supportedVersions": [
+          "v2"
+        ]
+      }
+      res.status(200).json(objret)
+});
+
+router.get('/protocol', (req, res) => {
+    objret = {
+        "version"   : "v2",  
+        "messageMethod" : "message",
+        "message_ContentType" : "application/json",
+        "supportedEncoding" : "BASE64",
+        "supportedCharacterSet" : "UTF-8"
+    }
+    res.status(200).json(objret)
+});
 
 
  router.post('/v2/discovery', (req, res)=> {
@@ -70,6 +101,21 @@ router.get('/test', (req,res) => {
 
     
     res.status(200).json(doc);
+});
+
+router.post('/v2/message', (req, res) => {
+    const objret = {
+        "status" : "OK", 
+        "message" : "Published successfully", 
+        "code" : 201        
+    }
+    const mess = req.body.document.value;
+
+    // code to manage the message goes here
+    
+    console.log(base64.decode(mess));
+
+    res.status(201).json(objret);
 });
 
 
